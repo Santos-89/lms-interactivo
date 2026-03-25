bb1f7e906122dd6021bfe2252f4da07058798a0b
josue otoniel mejia chacon <josueotonielmejiachacon@MacBook-Air-de-josue-1477.local>
2026-03-24 21:58:58 -0500

    🚀 config: Restaurar modo SSR con @cloudflare/next-on-pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 
 export const dynamic = 'force-dynamic';
-
-
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
